6532385e7de42f4057a520f219b5f835636e5b90
Imagineee <imagineeelego@gmail.com>
2022-06-17 20:49:58 +0100

    added in app about

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -347,3 +347,22 @@ reloadState(()=>{
 if (!localStorage.getItem('signInProvider')) {
 	MicroModal.show('account')
 }
+//About app section
+if (window.process) {
+	document.getElementById('desktop-ver').innerHTML = 'v'+window.require('electron').remote.app.getVersion()
+} else {
+	document.querySelectorAll(".desk-obj").forEach(e => {
+		e.style.display = 'none'
+	})
+}
+
+fetch('https://api.github.com/repos/imagineeeinc/paperclip/releases/latest')
+.then(res => {
+	return res.json()
+}).then((data) => {
+	document.getElementById('app-ver').innerHTML = data.tag_name
+	localStorage.setItem('appVersion', data.tag_name)
+}).catch(err => {
+	console.log(err)
+	document.getElementById('app-ver').innerHTML = localStorage.getItem('appVersion')
+})

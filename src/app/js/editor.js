c58dede277f7dab5e4e7d135be1002817882d333
Imagineee <59028351+imagineeeinc@users.noreply.github.com>
2022-12-01 20:18:29 +0000

    removed code formating

--- src/app/js/editor.js
+++ src/app/js/editor.js
@@ -3,7 +3,7 @@ var toolbarOptions = [
 	[{ 'header': 1 }, { 'header': 2 }], 
 	[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
-	['link','image','blockquote', 'code-block', { 'script': 'sub'}, { 'script': 'super' }],
+	['link','image','blockquote', { 'script': 'sub'}, { 'script': 'super' }],
   [{ 'list': 'ordered'}, { 'list': 'bullet' }],
   [{ 'indent': '-1'}, { 'indent': '+1' }],
   [{ 'direction': 'rtl' }, { 'align': [] }],
